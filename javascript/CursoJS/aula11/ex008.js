4075e07cbd1127b4dd3574a33fc611e988ff1ae7
Joao Junckes <joao1junckes@gmail.com>
2021-09-24 17:17:22 -0300

    Exercise
    More exercise on JS

---
+++ javascript/CursoJS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 80
+console.log(`A velocidade do seu carro e ${velocidade}km/h`)
+if (velocidade > 80) { //Condicao simples
+    console.log(`Voce ultrapasso a velocidade permitida!`)
+}
+console.log(`Dirija sempre usado cinto de seguranca!`)
