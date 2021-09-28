8e08eead2547f23386313f4440e080a74da1a1f8
Joao Junckes <joao1junckes@gmail.com>
2021-09-28 17:42:06 -0300

    While
    Learning to use while

---
+++ javascript/CursoJS/aula13/ambinte.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 1000)
+
+/*
+
+var c = 1
+while (c <= 5) {
+    console.log(`Tudo bem?`)
+    c++
+}
+
+*/
+
+/*
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
