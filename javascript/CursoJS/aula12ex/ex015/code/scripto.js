b7ff54eb176af3284875db1eccee2f963842d537
Joao Junckes <joao1junckes@gmail.com>
2021-09-28 16:54:49 -0300

    Update
    Update on the exercise

--- javascript/CursoJS/aula12ex/ex015/code/scripto.js
+++ javascript/CursoJS/aula12ex/ex015/code/scripto.js
@@ -17,14 +17,45 @@ function verificar() {
             var idade = ano - Number(formularioano.value)
             //res.innerHTML = `A sua idade e: ${idade} anos`
             var genero = ''
+            var img = document.createElement('img')
+            img.setAttribute(`id`, `foto`)
             
             if (fsex[0].checked) {
                 genero = 'Homem'
+                if (idade >= 0 && idade <10){
+                    //crianca
+                    img.setAttribute(`src`, `../images/recortadas/1.png`)
+                } else if (idade < 21){
+                    //jovem
+                    img.setAttribute(`src`, `../images/recortadas/2.png`)
+                } else if (idade < 50) {
+                    //adulto
+                    img.setAttribute(`src`, `../images/recortadas/3.png`)
+                } else {
+                    //idoso
+                    img.setAttribute(`src`, `../images/recortadas/4.png`)
+                }
+           
+           
             } else if (fsex[1].checked){
                 genero = 'Mulher'
+                if (idade >= 0 && idade <10){
+                    //crianca
+                    img.setAttribute('src', '../images/recortadas/5.png')
+                } else if (idade < 21){
+                    //jovem
+                    img.setAttribute('src', '../images/recortadas/6.png')
+                } else if (idade <50) {
+                    //adulta
+                    img.setAttribute('src', '../images/recortadas/7.png')
+                } else {
+                    //idosa
+                    img.setAttribute('src', '../images/recortadas/8.png')
+                }
             }
             res.style.textAlign = `center`
             res.innerHTML = `Voce e ${genero} com ${idade} anos`
+            res.appendChild(img)
         }
 
 
